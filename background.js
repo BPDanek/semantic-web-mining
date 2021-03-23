28f066899a11c9812b4904bafad4661135b24636
BPDanek <bdanek@asu.edu>
2021-03-22 20:08:00 -0700

    message passing example

--- background.js
+++ background.js
@@ -1,14 +1,16 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-        console.log('The color is green.');
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'developer.chrome.com'},
-            })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
+    chrome.history.search({
+        'text': '',              // Return every history item....
+        'maxResults': 100  // that was accessed less than one week ago.
+    }, function(historyItem) {
+        console.log(historyItem)
     });
 });
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.sendToDB == true)
+            console.log("sent response")
+            sendResponse({response: ("sendToDB reads TRRRRRRUUUEE ")});
+    }
+);
